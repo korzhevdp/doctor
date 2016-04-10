a78529b6a455baf52af28f2e7c5f380852a1ac0b
KorzhevDP <korzhevdp@gmail.com>
2016-04-10 02:02:48 +0200

    commit 3

--- application/views/welcome.php
+++
@@ -1,6 +0,0 @@
-<div style="height:500px;width:100%;text-align:center;vertical-align:middle;margin-top:150px;">
-	<h1 title="Система поддержки принятия решений">СППР&nbsp;&nbsp;<small>v 0.1</small></h1>
-	<h1><small>ООО "Балтийские реабилитационные технологии"</small></h1>
-	<a href="/login" class="btn btn-primary btn-large" style="margin-top:15px;margin-bottom:15px;">Войдите в систему</a><br>используя свои регистрационные данные<hr>
-	или посетите<br><a href="http://www.balt-reatech.ru" target="_blank" class="btn btn-info btn-large" style="margin-top:15px;margin-bottom:15px;">наш web-сайт</a>
-</div>
